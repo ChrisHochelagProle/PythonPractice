2489d8dabb5dca6a8a7e1f93dac73da9c276a886
hubris <desrosch@gmail.com>
2022-05-05 15:56:54 -0400

    Ajout d'explications en commentaire

--- tonAge2022.py
+++ tonAge2022.py
@@ -15,5 +15,8 @@ afficher "Vous avez: ", age, " ans!"
 anneeCourante = 2022
 
 anneeNaissance = input("Entrez votre annee de naissance: ")
+
+# Pour arriver à l'âge de la personne on soustrait l'année courante à son année de naissance. Si vous êtes né en 2000
+# Et qu'on est en 2022, vous avez donc 22 ans (2022 - 2000 donne bien 22)
 age = 2022 - int(anneeNaissance)
 print("Vous avez: " + str(age) + " ans!")
