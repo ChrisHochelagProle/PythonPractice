361ecd2f1d1fe154050eaa36ceb8625d769b63b3
hubris <desrosch@gmail.com>
2022-05-05 15:54:23 -0400

    Premier essai exemple avec l'age d'une personne en 2022

---
+++ tonAge2022.py
@@ -0,0 +1,19 @@
+'''''
+Nom de l'algorithme : tonAgeEn2022
+Constantes : annee = 2022 = entier
+Entrées : anneeLu = entier
+Sortie : age = entier
+
+afficher "Entrez votre annee de naissance"
+lire anneeLu
+age <-- 2022 - anneeLu
+afficher "Vous avez: ", age, " ans!"
+
+
+'''''
+
+anneeCourante = 2022
+
+anneeNaissance = input("Entrez votre annee de naissance: ")
+age = 2022 - int(anneeNaissance)
+print("Vous avez: " + str(age) + " ans!")
