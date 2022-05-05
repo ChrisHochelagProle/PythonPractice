9778c47f49cd25453bdb5e0373d2bbe89b9d4edf
hubris <desrosch@gmail.com>
2022-05-05 15:35:27 -0400

    ajout du pseudo code

--- prixParProvince.py
+++ prixParProvince.py
@@ -1,3 +1,24 @@
+# ********************************* PSEUDO CODE ************************************
+# taxe HST <-- 1.13
+# taxeTVQ <-- 0.09975
+# taxeTPS <-- 0.05
+#
+# afficher "Dans quelle province vivez vous? (Ontario ou Quebec)"
+# lire provinceLu
+# si provinceLu = Ontario
+#   afficher "Quel est le prix de l'article acheté?"
+#   lire prixLu
+#   prixTaxer <-- prixLu * taxeHST
+#   afficher "Le prix de l'article dans votre province est de:", prixTaxer, "$"
+# si provinceLu = Quebec
+#   afficher "Quel est le prix de l'article acheté?"
+#   lire prixLu
+#   prixTaxer <-- (prixLu * taxeTPS) + (prixLu * taxe TVQ) + prixLu
+#   afficher "Le prix de l'article dans votre province est de: ", prixTaxer, "$"
+# sinon
+#   afficher "Le programme n'implémente pas de fonction pour votre province
+# finsi
+
 taxeHST = 1.13
 taxeTVQ = 0.09975
 taxeTPS = 0.05
