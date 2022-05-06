b84fba6fa3c36ccb0bdfcd1168b70c84338e9eae
hubris <desrosch@gmail.com>
2022-05-06 10:23:29 -0400

    Manque le pseudo code - pratique pour examen

--- tableMultiplication.py
+++ tableMultiplication.py
@@ -1,3 +1,4 @@
+'''
 multiplicateur = 1
 print("Table de multiplication de 5")
 print("****************************************")
@@ -5,3 +6,18 @@ while multiplicateur <= 10:
     print(str(multiplicateur) + " * " + "5 = " + str(multiplicateur*5))
     multiplicateur += 1
 print("****************************************")
+'''
+
+#Version plus flexible
+multiplicateur = 1
+table_de = 5
+nb_multiplications = 4
+entete = "Table de multiplication de " + str(table_de)
+ligne_separation = "***************************"
+
+print(entete)
+print(ligne_separation)
+while multiplicateur <= nb_multiplications:
+    print(str(multiplicateur) + " * " + str(table_de) + " = " + str(multiplicateur * table_de))
+    multiplicateur += 1
+print(ligne_separation)
