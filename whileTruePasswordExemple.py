cf7f451c418bb470cb4c7e2cecbbb6af48baaacd
hubris <desrosch@gmail.com>
2022-05-09 15:21:58 -0400

    Complet - exemple 'a etudier' - Ajout de solutions alternatives et un niveau de complexité de plus avec un nombre d'essais

--- whileTruePasswordExemple.py
+++ whileTruePasswordExemple.py
@@ -15,9 +15,31 @@ tant que vrai faire
 
 """
 
+# Solution 1
+"""
 while True:
     mdp_Lu = input("Entrer un mot de passe: ")
     if mdp_Lu == "wow":
         print("Bienvenue.")
         break
     print("Le mot de passe est invalide.")
+"""
+
+# Solution 2
+"""
+mdp_Lu = input("Entrez un mot de passe: ")
+while mdp_Lu != "wow":
+    print("Mot de passe invalide")
+    mdp_Lu = input("Entrez un mot de passe: ")
+    
+print("Bienvenu")
+"""
+
+# Dans le cas ou l'utilisateur dépasse trois essais
+for iterateur in range(1,4):
+    mdp_Lu = input("Entrer un mot de passe: ")
+    if mdp_Lu == "wow":
+        print("Bienvenue.")
+        break
+    elif iterateur == 3:
+        print("Veuillez contacter votre superviseur")
