de73e6c06647a35f38594dc473283cdcdc15efe3
hubris <desrosch@gmail.com>
2022-05-13 10:40:25 -0400

    Finir plus tard

---
+++ Templates/templateExercice.py
@@ -0,0 +1,16 @@
+# Informations generales pour mise en page
+cours = "Concepts de programmation"
+nom = "Desrosiers Mondor"
+prenom = "Christopher"
+
+# Variables pour le pseudo code
+nom_de_algorithme = ""
+
+# Contantes
+
+# Entrees
+
+# Sorties
+
+# Mise en page
+message_ouverture = 30 * "*" + f""
