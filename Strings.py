2d889c1020747b9d1a65f426b95639b0b9ce8348
hubris <desrosch@gmail.com>
2022-05-13 10:47:23 -0400

    Exemple

--- Strings.py
+++ Strings.py
@@ -7,3 +7,18 @@ print("Bonjour"+" toi.")
 extract = string[0:5]
 
 print(extract)
+
+msg = "Bienvenu au cours {}"
+msg = msg.format("Intro a la programmation")
+print(msg)
+
+prenom = "chris"
+cours = "Intro a la programmation"
+msg = f"Bienvenu {prenom} dans le cours {cours}!"
+print(msg)
+print(msg.count(prenom))
+print(msg.upper())
+print(msg.lower())
+print(msg.replace(prenom, "JP"))
+print(msg.find(prenom))
+print(msg[9:])
