4d16baa274001b852a64b27a259490a8b9c98e8f
hubris <desrosch@gmail.com>
2022-05-13 11:04:44 -0400

    A finir

--- Initiales.py
+++ Initiales.py
@@ -19,9 +19,26 @@ print("Vos initiales: " + initiales)
 # Version qui prend en compte les noms composes - A finir plus tard.
 # nom = input("Entrez votre nom: ")
 nom = "Christopher Desrosiers Mondor"
+initiales = ""
+""" Tests
 print(nom.find(" "))
 partie1 = nom[0:11]
 reste = nom[12:]
 print(reste)
 nom = "Mondor"
 print(nom.find(" "))
+"""
+
+# Ne fonctionne pas
+"""
+while nom.find(" ") != -1 or nom != "":
+    partie_analyse = nom[0:nom.find(" ")]
+    print(partie_analyse)
+    initiale = partie_analyse[0]
+    indice_prochain_nom = int(nom.find(" ")) + 1
+    nom = nom[indice_prochain_nom:]
+    print(nom)
+    initiales += initiale
+
+print(initiales)
+"""
