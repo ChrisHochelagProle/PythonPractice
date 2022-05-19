e626a1a1af67a1f21525840431cd69e0f3bc12fa
hubris <desrosch@gmail.com>
2022-05-19 15:23:06 -0400

    A revoir

---
+++ inspection.py
@@ -0,0 +1,6 @@
+import inspect
+
+#code_count = inspect.getsource(str)
+# je peut imprimer le code source dun .py avec cette fonction juste remplacer str par le nom de la fonction
+# pas les fonctions built in comme str ...
+#print(code_count)
