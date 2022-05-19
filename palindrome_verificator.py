4b9540bd597c8870e8f64db086f077ed8edc46cd
hubris <desrosch@gmail.com>
2022-05-19 16:20:04 -0400

    Revoir pour l'examen - complet plus implementation de fonction

--- palindrome_verificator.py
+++ palindrome_verificator.py
@@ -22,6 +22,11 @@ afficher "Votre mot est un palindrome: ", is_palindrome, "."
 
 """
 
+# Imports
+import os.path
+
+# Fonctions
+
 
 def inverser(mot):
     mot_renverser = ""
@@ -30,6 +35,29 @@ def inverser(mot):
     return mot_renverser
 
 
+# Informations generales pour mise en page
+
+cours = "Concepts de programmation 1"
+nom = "Desrosiers Mondor"
+prenom = "Christopher"
+
+# Variables pour le pseudo code
+
+
+# Contantes
+nom_de_algorithme = os.path.basename(__file__)
+message_ouverture = 30 * "*" + f" Voici le resultat du programme: {nom_de_algorithme} " + 30 * "*"
+message_fermeture = 30 * "*" + f" Codé par {prenom} {nom} dans le cadre du cours {cours} " + 30 * "*"
+
+# Entrees
+
+# Sorties
+
+# Mise en page
+
+print(message_ouverture)
+
+# Mettre le code sous ce commentaire
 mot_entrer = input("Entrer un palindrome: ")
 # on gere si lutilisateur met un majuscule quelque part en mettant tout lowercase
 mot_a_evaluer = mot_entrer.lower()
@@ -44,3 +72,8 @@ if mot_a_evaluer == inverser(mot_a_evaluer):
     is_palindrome = True
 
 print("Votre mot est un palindrome?: ", is_palindrome)
+
+
+# Message footer
+print(message_fermeture)
+
