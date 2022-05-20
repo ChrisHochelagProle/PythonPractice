74ed316c9a31dcaaca9370720b89ad09bf0f0c30
hubris <desrosch@gmail.com>
2022-05-20 08:14:02 -0400

    Template general

--- Templates/templateExercice.py
+++ Templates/templateExercice.py
@@ -24,7 +24,7 @@ cours = "Concepts de programmation 1"
 nom = "Desrosiers Mondor"
 prenom = "Christopher"
 
-# Variables pour le pseudo code
+# Variables globales
 
 
 # Contantes
@@ -32,15 +32,12 @@ nom_de_algorithme = os.path.basename(__file__)
 message_ouverture = 30 * "*" + f" Voici le resultat du programme: {nom_de_algorithme} " + 30 * "*"
 message_fermeture = 30 * "*" + f" Codé par {prenom} {nom} dans le cadre du cours {cours} " + 30 * "*"
 
-# Entrees
-
-# Sorties
-
-# Mise en page
+# Message Header
 
 print(message_ouverture)
 
 # Mettre le code sous ce commentaire
+
 
 # Message footer
 print(message_fermeture)
