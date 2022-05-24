1633b0454b49de4a797776c448eda596fbfedcf2
hubris <desrosch@gmail.com>
2022-05-24 11:01:52 -0400

    Square method implementation

---
+++ Challenges/Challenge 23 square method.py
@@ -0,0 +1,17 @@
+"""
+Question 23
+level 1
+
+Question:
+    Write a method which can calculate square value of number
+
+Hints:
+    Using the ** operator
+"""
+
+
+def square(number):
+    return number ** 2
+
+
+print(square(4))
