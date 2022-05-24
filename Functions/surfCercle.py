b9b081afedb946fc52801767d4bbef47be96cead
hubris <desrosch@gmail.com>
2022-05-24 11:54:15 -0400

    Surface dun cercle

--- Functions/surfCercle.py
+++ Functions/surfCercle.py
@@ -4,9 +4,8 @@ import math
 
 def surfcercle(rayon):
     """Fonction qui renvoi l'aire dun cercle selon un rayon donnee"""
-    return math.pi*(rayon**2)
+    return math.pi * (rayon ** 2)
 
 
 print(surfcercle(2.5))
 print(surfcercle.__doc__)
-
