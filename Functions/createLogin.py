37347b7a3533d006fd11d8911530eb9c58818305
hubris <desrosch@gmail.com>
2022-05-26 15:43:33 -0400

    Fonction gen logins with name and integers

---
+++ Functions/createLogin.py
@@ -0,0 +1,12 @@
+"""
+Fonction qui cree un login avec un nom un prenom et deux entier. on utilier lentier un pour isoler les X premiers
+caracteres de nom et le deuxieme pour isoler les Y premier caracteres de prenom.
+
+Le resultat est une concatenation de ces deux chaines.
+"""
+
+def create_login(nom, prenom, taille1, taille2):
+    return nom[:taille1]+prenom[:taille2]
+
+
+print("Le login recommande est:", create_login("Christo", "Mondor", 3, 2))
