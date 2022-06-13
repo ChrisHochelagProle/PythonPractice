0b1c90ee8ccc4ab8bc46baeaca8ca0ed90adccbb
ChrisHochelag <desrosch@gmail.com>
2022-06-13 14:03:19 -0400

    Proper list printing - new print argument

--- Tableaux et listes/proper_list_printing.py
+++ Tableaux et listes/proper_list_printing.py
@@ -1,6 +1,5 @@
 t2 = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre']
 toPrint = ""
 for month in t2:
-    toPrint += month + " "
+    print(month, end=" ")
 
-print(toPrint)
