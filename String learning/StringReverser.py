5e073a981b20ff3eb8560329db42b9682ace0cc6
ChrisHochelag <desrosch@gmail.com>
2022-06-13 12:45:57 -0400

    String reverser - 2 ways

--- String learning/StringReverser.py
+++ String learning/StringReverser.py
@@ -1,7 +1,17 @@
+"""
+
 mot = input("Entrez un mot a renverser: ")
 mot_renverser = ""
 
 for compteur in range(1, len(mot)+1):
     mot_renverser += mot[len(mot)-compteur]
+
+print(mot_renverser)
+
+"""
+# Methode plus rapide.
+mot = input("Entrez un mot a renverser: ")
+# Ici on passe sur l'ensemble des caracteres avec un iterateur de -1 en partant de la fin.
+mot_renverser = mot[::-1]
 
 print(mot_renverser)
