bb14f3c1a104fe71f93869da909c1a016934f639
hubris <desrosch@gmail.com>
2022-06-20 13:54:21 -0400

    Exercices tuples

--- tuples/Fibonacci_sequence_tuples.py
+++ tuples/Fibonacci_sequence_tuples.py
@@ -2,6 +2,7 @@ def fibonacci(terme):
     sequence = (0, 1)
     result = 0
     # ici si je voulais ne pas afficher le terme dans les resultats il faudrait revoir la condition.
+    # fonctionne seulement si le terme est dans la sequence
     while result != terme:
         result = sequence[-2] + sequence[-1]
         sequence += (result,)
