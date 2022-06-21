80c5cb87cd698924c6a0cf075df11c362ba2d92e
hubris <desrosch@gmail.com>
2022-06-21 11:33:27 -0400

    Exercice Dict

---
+++ Dictionnaires/Exercice_4_langues_employes.py
@@ -0,0 +1,27 @@
+# On considère les données stockées dans les tuples suivants:
+# Les tuples e1, e2 et e3 contiennent les noms d'employés d'une succursale de banque (1e élément) suivi par
+# les langues maitrisées par cet employé
+# En utilisant un dictionnaire:
+# 1) affichez toutes langues avec lesquelles on peut servir la clientèle de la succursale
+# 2)  affichez le langage maitrisé par un maximum d'employé
+
+e1 = ("marco", "français", "anglais", "espagnol")
+e2 = ("Li", "français", "anglais", "mandarin")
+e3 = ("luca", "français", "italien")
+employes = e1, e2, e3
+
+
+def create_dict_of_speaking_language(employees):
+    dict_langues = {}
+    for workers in employees:
+        for info in workers[1:]:
+            if info not in dict_langues:
+                dict_langues[info] = 1
+            else:
+                dict_langues[info] += 1
+    return dict_langues
+
+
+dict_langue = create_dict_of_speaking_language(employes)
+print(dict_langue)
+print("La langue la plus parler par les employes: ", max(dict_langue, key=dict_langue.get))
