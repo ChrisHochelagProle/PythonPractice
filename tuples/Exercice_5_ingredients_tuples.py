1c0187462f2101d79d5b6f00d900f58bcfac2b9e
hubris <desrosch@gmail.com>
2022-06-21 09:35:44 -0400

    Exercices tuples

---
+++ tuples/Exercice_5_ingredients_tuples.py
@@ -0,0 +1,11 @@
+def recette_mult(ingredients, multi):
+    liste_ingredients = list()
+    for ingredient in ingredients:
+        active = list(ingredient)
+        active[1] = active[1] * 2
+        liste_ingredients.append(active)
+    return tuple(liste_ingredients)
+
+
+ingredients = (("Farine", 450), ("Oeuf", 4), ("Sucre", 420), ("Lait", 310))
+print(recette_mult(ingredients, 2))
