f82ae16be7aadd133e140f37c2bf5962a989494f
hubris <desrosch@gmail.com>
2022-06-21 10:11:11 -0400

    Exercices list

--- Tableaux et listes/insert_in_list.py
+++ Tableaux et listes/insert_in_list.py
@@ -3,9 +3,14 @@ def add_in_order(number, ordered_liste):
         if number <= ordered_liste[i]:
             ordered_liste.insert(i, number)
             break
+        else:
+            ordered_liste.append(number)
+            break
     return ordered_liste
 
 
 ordered_list = [14, 50, 100, 125, 500]
 
-print(add_in_order(50, ordered_list))
+
+
+print(add_in_order(510, ordered_list))
