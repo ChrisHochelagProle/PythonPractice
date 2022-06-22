45c8cc5ecc626f9cdf0e0fe95ec03b2731df3e22
hubris <desrosch@gmail.com>
2022-06-22 14:41:17 -0400

    Exercice String

--- String learning/extraires_mots_voyelles.py
+++ String learning/extraires_mots_voyelles.py
@@ -1,15 +1,15 @@
 def mots_avec_deux_voyelles(chaine):
-    list_voyelle = ["a", "e", "i", "o", "u", "y"]
+    list_voyelle = ("a", "e", "i", "o", "u", "y")
     liste_mots = chaine.lower().split()
     liste_mots_voyelles = []
     for mot in liste_mots:
-        second = False
+        second_vowel = False
         for lettre in mot:
-            if lettre in list_voyelle and second:
+            if lettre in list_voyelle and second_vowel:
                 liste_mots_voyelles.append(mot)
                 break
             elif lettre in list_voyelle:
-                second = True
+                second_vowel = True
     return liste_mots_voyelles
 
 
