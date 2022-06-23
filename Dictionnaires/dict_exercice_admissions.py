5f31465440e906f84820680c712008bc2f383e38
hubris <desrosch@gmail.com>
2022-06-23 14:38:45 -0400

    Exercice dict admissions

---
+++ Dictionnaires/dict_exercice_admissions.py
@@ -0,0 +1,19 @@
+def admissions(dict_etudiants):
+    dict_admis = {}
+    dict_non_admis = {}
+    for etudiant in dict_etudiants:
+        if dict_etudiants[etudiant] >= 10:
+            dict_admis[etudiant] = dict_etudiants[etudiant]
+        else:
+            dict_non_admis[etudiant] = dict_etudiants[etudiant]
+    return dict_admis, dict_non_admis
+
+
+etudiants = {"etudiant_1": 13, "etudiant_2": 17, "etudiant_3": 2, "etudiant_4": 17, "etudiant_5": 13,
+             "etudiant_6": 17, "etudiant_7": 5, "etudiant_8": 17, "etudiant_9": 8, "etudiant_10": 17,
+             "etudiant_11": 9, "etudiant_12": 17}
+
+tuple_of_dict_admissions = admissions(etudiants)
+
+print("Les eleves admis:", tuple_of_dict_admissions[0], "\n")
+print("Les eleces non admis:", tuple_of_dict_admissions[1])
