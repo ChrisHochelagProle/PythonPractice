67e28e9020e8f285d51abbe2d5c541dc672c142c
hubris <desrosch@gmail.com>
2022-06-27 13:27:49 -0400

    Init matrices

--- Matrices/init_matrice.py
+++ Matrices/init_matrice.py
@@ -1,2 +1,8 @@
-matrice = [[10 for i in range(5)], [20, 30, 40, 50, 60]]
+# 1 ligne en utilisant initialisation a la main
+matrice = [[10, 10, 10, 10, 10], [20, 30, 40, 50, 60]]
+assert matrice == [[10, 10, 10, 10, 10], [20, 30, 40, 50, 60]]
+
+# 1 ligne using list comprehension
+matrice = [[10 for i in range(5)], [i * 10 for i in range(2, 7)]]
+assert matrice == [[10, 10, 10, 10, 10], [20, 30, 40, 50, 60]]
 print(matrice)
