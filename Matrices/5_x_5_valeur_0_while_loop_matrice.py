fede42f7dbe117abfa25542cec36f28ab7aa4feb
hubris <desrosch@gmail.com>
2022-06-27 13:13:03 -0400

    Init 0 with while loop matrice 5x5

---
+++ Matrices/5_x_5_valeur_0_while_loop_matrice.py
@@ -0,0 +1,12 @@
+matrice = []
+ligne = 0
+while ligne < 5:
+    liste = []
+    colonne = 0
+    while colonne < 5:
+        liste.append(0)
+        colonne += 1
+    matrice.append(liste)
+    ligne += 1
+
+print(matrice)
