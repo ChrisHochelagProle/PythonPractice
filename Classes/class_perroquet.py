4ea60b585dc82cd66a1e427f3a9eb564e1073982
hubris <desrosch@gmail.com>
2022-06-28 11:01:15 -0400

    import test

--- Classes/class_perroquet.py
+++ Classes/class_perroquet.py
@@ -1,17 +1,18 @@
 class Perroquet:
 
-
     def __init__(self, nom, age):
         self.nom = nom
         self.age = age
         self.altitude = 0
-
 
     def repeter(self, phrase):
         print(self.nom + " dit: " + phrase * 3)
 
     def set_nom(self, new_name):
         self.nom = new_name
+
+    def envoleToi(self, altitude):
+        self.altitude = altitude
 
 perroquet1=Perroquet("Terreur", 3)
 perroquet1.repeter("Bonjour les pirates! ")
