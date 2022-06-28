b7ad838577e09d0f57e74b45bb3cf9da2e85aeab
hubris <desrosch@gmail.com>
2022-06-28 09:25:15 -0400

    Exemple

---
+++ modules/import_tutorial.py
@@ -0,0 +1,7 @@
+# importing sys
+import sys
+
+# adding Folder_2 to the system path
+sys.path.insert(0, 'C:\\Users\\e2295874\\PycharmProjects\\PythonPractice\\modules')
+
+from utilities import is_prime
