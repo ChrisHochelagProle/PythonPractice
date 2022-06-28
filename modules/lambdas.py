ced7902dfb2ceab0702d8f3774f0395f2643c840
hubris <desrosch@gmail.com>
2022-06-28 09:37:07 -0400

    Lambdas to use

--- modules/lambdas.py
+++
@@ -1,7 +0,0 @@
-GREATERTHAN = lambda a, b: a > b
-GREATEROREQUAL = lambda a, b: a >= b
-LOWERTHAN = lambda a, b: a < b
-LOWEROREQUAL = lambda a, b: a <= b
-'''Returns bool'''
-EQUAL = lambda a, b: a == b
-NOTEQUAL = lambda a, b: a != b
