b85520705fbe78fdd0f82b159d07310596551b35
hubris <desrosch@gmail.com>
2022-06-28 11:53:16 -0400

    Compte bancaire

---
+++ Classes/class_compte_bancaire.py
@@ -0,0 +1,42 @@
+class Compte:
+    def __init__(self):
+        self.solde = 0
+
+    def __str__(self):
+        return f'Solde: {self.solde}'
+
+    def depot(self, montant: float):
+        if montant > 0:
+            self.solde += montant
+        else:
+            print("Montant invalide")
+
+    def retrait(self, montant: float):
+        if montant > 0:
+            self.solde -= montant
+        else:
+            print("Montant invalide")
+
+list_comptes = []
+for i in range(10):
+    compte = Compte()
+    depot = float(input("Combien voulez-vous mettre dans le compte: "))
+    compte.depot(depot)
+    list_comptes.append(compte)
+
+moyenne = 0
+list_soldes = []
+for compte in list_comptes:
+    moyenne += compte.solde
+    list_soldes.append(compte.solde)
+moyenne = moyenne/len(list_comptes)
+
+greater_30k = lambda solde : solde > 30000
+lesser_20k = lambda solde : solde < 20000
+
+nombre_30k = len(list(filter(greater_30k, list_soldes)))
+nombre_20k = len(list(filter(lesser_20k, list_soldes)))
+
+print("La moyenne des soldes des comptes dans notre banque", moyenne, "$")
+print("Compte de plus de 30k:", nombre_30k)
+print("Compte de moins de 20k:", nombre_20k)
