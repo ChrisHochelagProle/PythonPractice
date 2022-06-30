4970de1cc6e6e1ba2f28ba18ebca532244006925
hubris <desrosch@gmail.com>
2022-06-30 16:44:30 -0400

    #Done

--- Classes/test_trajet.py
+++ Classes/test_trajet.py
@@ -17,3 +17,28 @@ trajet_mtl_qc.avance(150)
 trajet_mtl_qc.debarque(4)
 print(trajet_mtl_qc)
 print(trajet_mtl_qc.info())
+
+# Output
+# <class 'class_Trajet.Trajet'>
+# Ville de depart: Montreal
+# Ville d'arriver: Quebec
+# Nombre de passagers: 2
+# Kms parcourus: 0
+# <class 'class_Trajet.Trajet'>
+# Ville de depart: Montreal
+# Ville d'arriver: Quebec
+# Nombre de passagers: 2
+# Kms parcourus: 150
+# <class 'class_Trajet.Trajet'>
+# Ville de depart: Montreal
+# Ville d'arriver: Quebec
+# Nombre de passagers: 4
+# Kms parcourus: 150
+# <class 'class_Trajet.Trajet'>
+# Ville de depart: Montreal
+# Ville d'arriver: Quebec
+# Nombre de passagers: 0
+# Kms parcourus: 300
+#
+# Process finished with exit code 0
+
