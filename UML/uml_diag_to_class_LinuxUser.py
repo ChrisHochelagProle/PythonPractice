863856d4a3d1ca7b21d683431937643327482ad2
hubris <desrosch@gmail.com>
2022-07-12 09:31:16 -0400

    #Done

---
+++ UML/uml_diag_to_class_LinuxUser.py
@@ -0,0 +1,21 @@
+class UtilisateurLinux:
+    def __init__(self, id: str, uid: str, home_dir: str, list_groups: list):
+        self.identifiant = id
+        self.uid = uid
+        self.repertoirePerso = home_dir
+        self.membreDe = list_groups
+
+    def connecter(self):
+        pass
+
+    def deconnecter(self):
+        pass
+
+    def getUID(self):
+        return self.uid
+
+    def ajouterGroupe(self, nom_groupe: str):
+        self.membreDe.append(nom_groupe)
+
+    def supprimerGroupe(self, nom_groupe: str):
+        self.membreDe.remove(nom_groupe)
