bb0bcd93f0fc43cd40a3403c858750fab7efeb64
ChrisHochelag <desrosch@gmail.com>
2022-07-12 13:19:55 -0400

    #Tofinish

--- UML/uml_to_class_generator.py
+++ UML/uml_to_class_generator.py
@@ -1,47 +1,93 @@
+def returnAlpha(string: str):
+    result = ""
+    for char in string:
+        if char.isalpha():
+            result += char
+    return result
+
+class Attribut:
+    def __init__(self, security, name, typeOf):
+        self.private = security
+        self.nom = name
+        self.type = typeOf
+
+    def afficher(self):
+        return (self.nom, self.private, self.type)
+class Methode:
+    def __init__(self, security, name, retourOf):
+        self.private = security
+        self.nom = name
+        self.retour = retourOf
+
+    def afficher(self):
+        return (self.nom, self.private, self.retour)
+
 class ClassGenerator:
     def __init__(self, uml: str):
         self.uml_diagram = uml
 
     def generateClass(self):
         nom_de_classe = ""
-        list_attributs = []
-        list_methodes = []
         lines_in_uml = self.uml_diagram.split("\n")
-        compteur_separations = 0
+        lines_without_bars = []
         for line in lines_in_uml:
+            lines_without_bars.append(line.strip("|"))
+        compteur_separations = 0
+        attributs = []
+        methodes = []
+        for line in lines_without_bars:
+            private = False
+            nom_attribut = ""
+            type_attribut = ""
+            nom_methode = ""
+            type_retour = ""
             if "*" in line:
                 compteur_separations += 1
                 continue
             if compteur_separations == 1:
-                line.strip(' ')
-                line.strip('|')
-                nom_de_classe = line
+                pass
+                nom_de_classe = line.strip(" ")
             elif compteur_separations == 2:
-                line.strip('|')
-                new_string = line[1:]
-                isnotspace = lambda char: char != " "
-                for char in new_string[-1:-len(line)]:
-                    if isnotspace(char):
-                        break
-                    else:
-                        new_string = new_string[0:len(new_string) - 2]
-                list_attributs.append(new_string)
+                if "-" in line:
+                    private = True
+                elif "+" in line:
+                    private = False
+                list_nom_et_type = line.split(":")
+                nom_pre_edit = list_nom_et_type[0]
+                type_pre_edit = list_nom_et_type[1]
+                nom_attribut = returnAlpha(nom_pre_edit)
+                type_attribut = returnAlpha(type_pre_edit)
+                attribut = Attribut(private, nom_attribut, type_attribut)
+                attributs.append(attribut)
             elif compteur_separations == 3:
-                line.strip('|')
-                new_string = line[1:]
-                isnotspace = lambda char: char != " "
-                for char in new_string[-1:-len(line)]:
-                    if isnotspace(char):
-                        break
-                    else:
-                        new_string = new_string[0:len(new_string)-2]
-                list_methodes.append(new_string)
+                if "-" in line:
+                    private = True
+                elif "+" in line:
+                    private = False
+                list_nom_et_type = line.split(":")
+                nom_pre_edit = list_nom_et_type[0]
+                retour_pre_edit = list_nom_et_type[1]
+                nom_methode = returnAlpha(nom_pre_edit)
+                type_retour = returnAlpha(type_pre_edit)
+                methode = Methode(private, nom_methode, type_retour)
+                methodes.append(methode)
             else:
                 break
-        infos = f'Nom: {nom_de_classe}\n' \
-                f'Attributs: {list_attributs}\n' \
-                f'Methodes: {list_attributs}'
-        return infos
+        attributs_text = ""
+        for atribu in attributs:
+            if atribu.private:
+                attributs_text += f"    self.__{atribu.nom} = ''\n    "
+        methode_text = ""
+        for method in methodes:
+            methode_text += f"def {method.nom}(self):\n    " \
+                            f"    pass"
+        class_body = f"class {nom_de_classe}:\n" \
+                f"    def __init__(self):\n" \
+                f"    {attributs_text}\n" \
+                f"\n" \
+                f"    {methode_text}"
+
+        return class_body
 
 
 uml = f'| ************** |\n' \
@@ -54,36 +100,5 @@ uml = f'| ************** |\n' \
       f'| + infos(): str |\n' \
       f'| ************** |\n'
 
-# generator = ClassGenerator(uml)
-# print(generator.generateClass())
-
-lines_in_uml = uml.split("\n")
-print(lines_in_uml)
-lines_without_bars = []
-for line in lines_in_uml:
-    lines_without_bars.append(line.strip("|"))
-print(lines_without_bars)
-compteur_separations = 0
-nom_de_classe = ""
-list_attribut = []
-for line in lines_without_bars:
-    private = False
-    nom_attribut = ""
-    type_attribut = ""
-    if "*" in line:
-        compteur_separations += 1
-        continue
-    if compteur_separations == 1:
-        pass
-        nom_de_classe = line.strip(" ")
-    elif compteur_separations == 2:
-        if "-" in line:
-            private = True
-        elif "+" in line:
-            private = False
-
-    elif compteur_separations == 3:
-        pass
-    else:
-        break
-print(nom_de_classe)
+generator = ClassGenerator(uml)
+print(generator.generateClass())
